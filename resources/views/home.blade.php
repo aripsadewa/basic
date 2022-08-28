c00216c7ae1eec4535ffae2b07ed17e6699ca485
Arip Sadewa <find.arip@gmail.com>
2022-08-28 23:28:31 +0700

    blade themplate

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout title="Home">
+{{-- @component('components.alert')
+        @slot('title')
+            header slot
+        @endslot
+        isinya
+        @slot('footer')
+        footer slot
+        @endslot
+    @endcomponent --}}
+    Home Page
+</x-app-layout>
+
+
